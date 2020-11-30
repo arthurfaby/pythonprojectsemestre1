414cc949faf66ae387fff234a5d6fc608b7ca010
arthurfaby <arthfaby33@gmail.com>
2020-11-30 18:31:42 +0100

    Add some functions and clear the app file

--- app.py
+++ app.py
@@ -1,35 +1,6 @@
 import functions as f
 
-partitions = open("partitions.txt", "r", encoding="utf-8")
+dic_title_and_writed_notes = f.set_dic_title_and_writed_notes()
+dic_title_and_numerical_notes = f.dic_title_and_numerical_notes()
 
-dic = {}
-titles = []
-notes = []
-
-
-
-
-L = partitions.read()
-L = L.split("\n")
-
-partitions.close()
-for i in range(1,len(L)+1):
-    if i%2 == 1:
-        title_init = L[i-1]
-        title_init = title_init[title_init.index(' ')+1:]
-        titles.append(title_init)
-    else:
-        notes_init = L[i-1]
-        notes.append(notes_init)
-        dic[title_init] = notes_init
-        
-
-
-print(f.get_notes_and_durations(dic,  "Au feu les pompiers"))
-
-#dic = {}
-#dic["Frère Jacques"] = "SOL"
-#dic["Une souris verte"] = "FA"
-#str = "1 Bonjour"
-#str = str[str.index(" ")+1:]
-#print(str)
+print()
--- functions.py
+++ functions.py
@@ -3,6 +3,26 @@ import numpy as np
 import simpleaudio as sa
 import turtle as tr
 
+def set_dic_title_and_writed_notes():
+    partitions = open("partitions.txt", "r", encoding="utf-8")
+    dic_title_and_writed_notes = {}
+    titles = []
+    notes = []
+    L = partitions.read()
+    L = L.split("\n")
+    for i in range(1,len(L)+1):
+        if i%2 == 1:
+            title_init = L[i-1]
+            title_init = title_init[title_init.index(' ')+1:]
+            titles.append(title_init)
+        else:
+            notes_init = L[i-1]
+            notes.append(notes_init)
+            dic_title_and_writed_notes[title_init] = notes_init
+    return dic_title_and_writed_notes
+
+
+
 def sound(freq,duration):
     sample_rate=44100
     t=np.linspace(0, duration, int(duration*sample_rate), False)
@@ -19,46 +39,47 @@ def sound(freq,duration):
     play_obj=sa.play_buffer(audio,1,3,sample_rate)
     play_obj.wait_done()
 
-def get_notes(dic, title):
-    notes = dic[title]
+def get_notes(dic_title_and_writed_notes, title):
+    notes = dic_title_and_writed_notes[title]
     notes = notes.split(" ")
     return notes
 
-def get_notes_and_type_of_duration(dic, title):
+def get_notes_and_type_of_duration(dic_title_and_writed_notes, title):
     
     notes_and_type_of_duration = []
-    for i in get_notes(dic, title):
+    for i in get_notes(dic_title_and_writed_notes, title):
         duration = i[len(i)-1]
         i = i[0:len(i)-1]
         notes_and_type_of_duration.append(i)
         notes_and_type_of_duration.append(duration)
     return notes_and_type_of_duration
 
-def get_notes_and_durations(dic, title):
-    L = get_notes(dic, title)
+def get_notes_and_durations(dic_title_and_writed_notes, title):
+    L = get_notes(dic_title_and_writed_notes, title)
     notes_and_durations = [[],[]]
-    for i in get_notes(dic, title):
+    for i in get_notes(dic_title_and_writed_notes, title):
         duration = i[len(i)-1]
         note = i[0:len(i)-1]
 
-        # We transform the note by the associated frequency in Hz
+        # We transform the note by the associated a number
 
         if note == "DO":
-            note = 264
+            note = 1
         elif note == "RE":
-            note = 297
+            note = 2
         elif note == "MI":
-            note = 330
+            note = 3
         elif note == "FA":
-            note = 352
+            note = 4
         elif note == "SOL":
-            note = 396
+            note = 5
         elif note == "LA":
-            note = 440
+            note = 6
         elif note == "SI":
-            note = 495
+            note = 7
 
         # We transform the letter by the associated time in ms
+
 
         if duration == "c":
             duration = 125
@@ -73,3 +94,10 @@ def get_notes_and_durations(dic, title):
         notes_and_durations[1].append(duration)
 
     return notes_and_durations
+
+
+def add_partition(title, notes):
+    partitions = open("partitions.txt", "a", encoding="utf-8")
+    partitions.write("\n" + title + "\n")
+    partitions.write(notes)
+
